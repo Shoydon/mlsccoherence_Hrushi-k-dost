a2edbe2e509f4269957fad7375d92ad7695fb1a2
wesley-lewis <lewis.wes.23@gmail.com>
2024-03-23 12:02:18 +0530

    structure of few pages added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes } from "react-router"
+import { BrowserRouter, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Payments from "./components/Payments"
+import Signin from "./components/Signin"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route index element={<Home />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home page</h1>
+        <p>Hrushi k Dost</p>
+    </div>
+  )
+}
+
+export default Home
